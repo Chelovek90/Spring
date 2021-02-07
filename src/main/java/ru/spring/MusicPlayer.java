5914d11992a62c8bd5eb65049c9a31adb1046541
Alexandr Shadskiy <homosapiens-90@mail.ru>
2021-02-07 13:47:37 +0300

    "Dependency Injection - setter"

--- src/main/java/ru/spring/MusicPlayer.java
+++ src/main/java/ru/spring/MusicPlayer.java
@@ -3,11 +3,37 @@ package ru.spring;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
+    public MusicPlayer() {
+    }
+
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
     public void playMusic() {
         System.out.println(music.getSong());
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
