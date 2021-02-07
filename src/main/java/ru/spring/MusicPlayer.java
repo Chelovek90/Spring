e3ce6f66abedc75eeb9e0424d0ecdecdaad43e2b
Alexandr Shadskiy <homosapiens-90@mail.ru>
2021-02-07 14:04:36 +0300

    "Dependency Injection - setter_List"

--- src/main/java/ru/spring/MusicPlayer.java
+++ src/main/java/ru/spring/MusicPlayer.java
@@ -1,7 +1,10 @@
 package ru.spring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
 
     private String name;
     private int volume;
@@ -9,31 +12,29 @@ public class MusicPlayer {
     public MusicPlayer() {
     }
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public void playMusicList() {
+        for (Music music:musicList) {
+            System.out.println(music.getSong());;
+        }
     }
 
-    public void playMusic() {
-        System.out.println(music.getSong());
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public String getName() {
-        return name;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public int getVolume() {
-        return volume;
-    }
-
     public void setVolume(int volume) {
         this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
     }
 }
--- src/main/java/ru/spring/TestSpring.java
+++ src/main/java/ru/spring/TestSpring.java
@@ -9,7 +9,8 @@ public class TestSpring {
         );
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        musicPlayer.playMusic();
+        musicPlayer.playMusicList();
+
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
 
