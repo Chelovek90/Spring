a61ebf8360adffe36f825ab7d210866ca73136af
Alexandr Shadskiy <homosapiens-90@mail.ru>
2021-02-07 20:12:09 +0300

    "Annotation - @Autowired_private field"

--- src/main/java/ru/spring/MusicPlayer.java
+++ src/main/java/ru/spring/MusicPlayer.java
@@ -9,14 +9,9 @@ import java.util.List;
 @Component
 public class MusicPlayer {
 
+    @Autowired
     private Music music;
 
-    @Autowired
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    @Autowired
     public MusicPlayer(Music music) {
         this.music = music;
     }
