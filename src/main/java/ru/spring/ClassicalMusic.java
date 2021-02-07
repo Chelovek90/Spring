36239567531ce3f5324527fbbc6cfd9059c5654a
Alexandr Shadskiy <homosapiens-90@mail.ru>
2021-02-07 19:17:52 +0300

    "Factory method"

--- src/main/java/ru/spring/ClassicalMusic.java
+++ src/main/java/ru/spring/ClassicalMusic.java
@@ -1,6 +1,13 @@
 package ru.spring;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic() {
+    }
+
+    public static  ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
     public void myInitMethod() {
         System.out.println("Classical init");
     }
